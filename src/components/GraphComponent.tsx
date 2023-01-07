6c6e1f68594ce30734d29886d642210593f46989
rsdoc <rscodedocs@gmail.com>
2023-01-07 22:21:41 +0530

    removed error

--- src/components/GraphComponent.tsx
+++ src/components/GraphComponent.tsx
@@ -10,12 +10,6 @@ export default function D3Graph({ queryResponse }: any) {
     const currentQuery = queriesList.filter(
       (query: any) => parseInt(query.id) === parseInt(id)
     )[0];
-
-    d3Sparql('https://dbpedia.org/sparql', currentQuery?.query)
-      .then((data: any) => console.log(data, 'data....'))
-      .catch((error: any) => console.log(error));
-
-    console.log('currentQuery...', currentQuery);
   });
   return <h1>I am graph component</h1>;
 }
